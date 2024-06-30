6b353676b4017ec677a0f2c092935b414622c546
Charles K. Neimog <charlesneimog@outlook.com>
2024-06-30 11:08:26 -0300

    Update dotfiles

--- Config/nvim/lua/config/plugins/latex.lua
+++ Config/nvim/lua/config/plugins/latex.lua
@@ -15,7 +15,7 @@ return {
 		},
 		config = function()
 			vim.g.vimtex_view_general_viewer = "okular"
-			vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+			vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
 
 			vim.g.vimtex_compiler_latexmk = {
 				backend = "nvim",
