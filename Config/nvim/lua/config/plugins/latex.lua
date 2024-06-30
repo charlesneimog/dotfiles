720bee186cdeeb8cb7d9006e4a23ecb0c1d41812
Charles K. Neimog <charlesneimog@outlook.com>
2024-06-30 11:03:16 -0300

    Update dotfiles

--- Config/nvim/lua/config/plugins/latex.lua
+++ Config/nvim/lua/config/plugins/latex.lua
@@ -14,7 +14,6 @@ return {
 			},
 		},
 		config = function()
-			vim.g.vimtex_view_method = "skim"
 			vim.g.vimtex_view_general_viewer = "okular"
 			vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
 
