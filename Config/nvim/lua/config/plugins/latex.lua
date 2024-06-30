d9b5a65f3805ddc748d54c81525115fd99eb48ab
Charles K. Neimog <charlesneimog@outlook.com>
2024-06-30 11:06:37 -0300

    Update dotfiles

--- Config/nvim/lua/config/plugins/latex.lua
+++ Config/nvim/lua/config/plugins/latex.lua
@@ -50,28 +50,28 @@ return {
 				"Package hyperref Warning: Token not allowed in a PDF string",
 				[[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in paragraph at lines]],
 			}
-			-- Check if the global variable doesn't exist
-			if not vim.g.vim_window_id then
-				-- Set the global variable using the result of the system command
-				vim.g.vim_window_id = vim.fn.system("xdotool getactivewindow")
-			end
-
-			-- Define a Lua function for TexFocusVim
-			local function TexFocusVim()
-				-- Give window manager time to recognize focus moved to Zathura
-				-- Tweak the delay (200) as needed for your hardware and window manager
-				vim.fn.sleep(200)
-
-				-- Refocus Vim and redraw the screen
-				vim.fn.system("!xdotool windowfocus " .. vim.fn.expand(vim.g.vim_window_id))
-				vim.cmd("redraw!")
-			end
-
-			-- Make the Lua function accessible within Vimscript
-			vim.api.nvim_set_var("TexFocusVim", TexFocusVim)
-
-			-- Map the function to a command (optional)
-			vim.cmd("command! TexFocusVim lua TexFocusVim()")
+			-- -- Check if the global variable doesn't exist
+			-- if not vim.g.vim_window_id then
+			-- 	-- Set the global variable using the result of the system command
+			-- 	vim.g.vim_window_id = vim.fn.system("xdotool getactivewindow")
+			-- end
+			--
+			-- -- Define a Lua function for TexFocusVim
+			-- local function TexFocusVim()
+			-- 	-- Give window manager time to recognize focus moved to Zathura
+			-- 	-- Tweak the delay (200) as needed for your hardware and window manager
+			-- 	vim.fn.sleep(200)
+			--
+			-- 	-- Refocus Vim and redraw the screen
+			-- 	vim.fn.system("!xdotool windowfocus " .. vim.fn.expand(vim.g.vim_window_id))
+			-- 	vim.cmd("redraw!")
+			-- end
+			--
+			-- -- Make the Lua function accessible within Vimscript
+			-- vim.api.nvim_set_var("TexFocusVim", TexFocusVim)
+			--
+			-- -- Map the function to a command (optional)
+			-- vim.cmd("command! TexFocusVim lua TexFocusVim()")
 		end,
 	},
 }
