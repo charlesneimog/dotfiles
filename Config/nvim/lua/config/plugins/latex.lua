2ffc1d0b5f8306efc6a5947c4fbd028c46a1a810
Charles K. Neimog <charlesneimog@outlook.com>
2024-06-30 11:19:06 -0300

    Update dotfiles

--- Config/nvim/lua/config/plugins/latex.lua
+++ Config/nvim/lua/config/plugins/latex.lua
@@ -14,7 +14,7 @@ return {
 			},
 		},
 		config = function()
-			vim.g.vimtex_view_general_viewer = "okular"
+			vim.g.vimtex_view_general_viewer = "xdg-open"
 			vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
 
 			vim.g.vimtex_compiler_latexmk = {
