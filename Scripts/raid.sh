8e0045e1d74f8f336be0208093298fe1ddcd145b
Charles K. Neimog <charlesneimog@outlook.com>
2024-07-12 09:54:57 -0300

    Update dotfiles

--- Scripts/raid.sh
+++ Scripts/raid.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-export=DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-export=DISPLAY=:0
 
-# Verifica se os dispositivos estão conectados
-if lsusb | grep -q "152d:0576" && lsusb | grep -q "152d:2329"; then
-    notify-send "Ambos os dispositivos JMicron estão conectados!"
-else
-    notify-send "Aguardando ambos os dispositivos JMicron estarem conectados..."
-fi
+# Dentro do script raid.sh
+echo "Script raid.sh executado em $(date)" >> /tmp/raid_trigger.log
 
