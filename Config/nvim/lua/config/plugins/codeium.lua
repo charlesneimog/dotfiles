bde1d48fff96313e518f6b8ac920862d1cbbcc47
Charles K. Neimog <charlesneimog@outlook.com>
2024-10-01 08:22:46 -0300

    Update dotfiles

--- Config/nvim/lua/config/plugins/codeium.lua
+++ Config/nvim/lua/config/plugins/codeium.lua
@@ -4,7 +4,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"hrsh7th/nvim-cmp",
 	},
-	config = function()
-		require("codeium").setup({})
-	end,
+	-- config = function()
+	-- 	require("codeium").setup({})
+	-- end,
 }
