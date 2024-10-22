de9ae323beb4c7d2e85cb25ba5453a47d33489ef
Charles K. Neimog <charlesneimog@outlook.com>
2024-10-22 09:03:14 -0300

    Update dotfiles

--- Config/nvim/lua/config/plugins/onedark.lua
+++ Config/nvim/lua/config/plugins/onedark.lua
@@ -52,4 +52,20 @@ return {
 			end
 		end,
 	},
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "auto",
+				background = {
+					light = "latte",
+					dark = "mocha",
+				},
+				color_overrides = {
+					mocha = { bg = "#303030", fg = "#ffffff", fg_gutter = "#ffffff" },
+				},
+			})
+		end,
+	},
 }
