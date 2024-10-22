1a202d60304ef65c03c4ef6f2b33f3402ede0ba0
Charles K. Neimog <charlesneimog@outlook.com>
2024-10-22 08:53:49 -0300

    Update dotfiles

--- Config/nvim/lua/config/plugins/onedark.lua
+++ Config/nvim/lua/config/plugins/onedark.lua
@@ -39,8 +39,8 @@ return {
 		local theme = getGnomeThemeMode()
 		require("onedarkpro").setup({
 			colors = {
-				onedark = { bg = "#303030", fg = "#000000", float_bg = "#2e2e2e" },
-				onelight = { bg = "#FFFFFF", float_bg = "#fcfcfc" },
+				onedark = { bg = "#303030", float_bg = "#2e2e2e" },
+				onelight = { bg = "#FFFFFF", fg = "#000000", float_bg = "#fcfcfc" },
 			},
 		})
 		if theme == "dark" then
