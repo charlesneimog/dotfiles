5ab77f8b5542546187143dd2e6577590793790ca
Charles K. Neimog <charlesneimog@outlook.com>
2024-11-02 13:49:36 -0300

    Update dotfiles

--- Scripts/delete-github-workflows.sh
+++ Scripts/delete-github-workflows.sh
@@ -1,11 +1,11 @@
 OWNER=charlesneimog
 REPO=pd4web
+WORKFLOW_ID=Wheels.yml
 
 # list workflows
 gh api -X GET /repos/$OWNER/$REPO/actions/workflows | jq '.workflows[] | .name,.id'
 
 # copy the ID of the workflow you want to clear and set it
-WORKFLOW_ID=pd4web-tests.yml
 
 # list runs
 gh api -X GET /repos/$OWNER/$REPO/actions/workflows/$WORKFLOW_ID/runs --paginate | jq '.workflow_runs[] | .id'
