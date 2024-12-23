06243ea579edbed249e9d72f0ed008b6a7d6c3be
Charles K. Neimog <charlesneimog@outlook.com>
2024-12-23 12:08:51 -0300

    Update dotfiles

--- Config/nvim/lua/config/plugins/lsp.lua
+++ Config/nvim/lua/config/plugins/lsp.lua
@@ -63,20 +63,21 @@ return {
 				},
 			}
 
-			-- ??
+			-- use default
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local mason_lspconfig = require("mason-lspconfig")
 			mason_lspconfig.setup({
 				ensure_installed = vim.tbl_keys(servers),
 				automatic_installation = true,
 			})
-
-			for server_name, _ in pairs(servers) do
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-					on_attach = on_attach, --
-					settings = servers[server_name],
-				})
-			end
+			mason_lspconfig.setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+						settings = servers[server_name],
+					})
+				end,
+			})
 		end,
 	},
 	{
